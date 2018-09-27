58867f3a4c8bec8973d344a4b291c37fb38ad938
persian815 <persian815@gmail.com>
2018-09-27 11:24:45 +0900

    commit

---
+++ testApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testApp
+//
+//  Created by LeeSeongRyong on 2018. 9. 27..
+//  Copyright © 2018년 LeeSeongRyong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testApp
+//
+//  Created by LeeSeongRyong on 2018. 9. 27..
+//  Copyright © 2018년 LeeSeongRyong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
